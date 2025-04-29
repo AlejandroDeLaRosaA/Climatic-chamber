aac3b426199d6b34d6f2422abd7b266c86dd24b9
Alejandro DLR <114164738+AlejandroDeLaRosaA@users.noreply.github.com>
2025-04-28 20:00:53 -0600

    Added pins for additional fans and features

--- ESP32_ClimaticChamber_Release.ino
+++ ESP32_ClimaticChamber_Release.ino
@@ -4,17 +4,31 @@
 #include <DHT.h>
 #include <DHT_U.h>
 
-/* === Relays === */
-#define RELAY1_PIN 33
-#define RELAY2_PIN 26
-#define RELAY3_PIN 27
-#define RELAY4_PIN 13
+/* === Features that need relays === */
+#define PELTIER_HOT_PIN       33
+#define PELTIER_HOT_FAN_PIN   26
+#define PELTIER_COLD_PIN      27
+#define PELTIER_COLD_FAN_PIN  14
+#define WATER_PUMP_PIN        13
+#define RGB_STRIPE_RED_PIN    23
+#define RGB_STRIPE_BLUE_PIN   22
+#define RGB_STRIPE_GREEN_PIN  4
+
+/* === Relay logic === */
+#define ON 0x0
+#define OFF 0x1
 
 /* === Ground Humidity Sensors === */
 #define HUM_SENSOR1_PIN 36
 #define HUM_SENSOR2_PIN 39
 #define HUM_SENSOR3_PIN 34
 #define HUM_SENSOR4_PIN 35
+
+/* === DHT11 Sensors pins === */
+#define DHT11_1_PIN 21
+#define DHT11_2_PIN 19
+#define DHT11_3_PIN 17
+#define DHT11_4_PIN 16
 
 /* === ADC === */
 #define ADC_PIN 32
@@ -26,74 +40,186 @@ float voltage = 0.0;
 float dacValue = 0.0;
 
 /* === DHT11 === */
-DHT dht1(21, DHT11);   // GPIO21
-DHT dht2(19, DHT11);   // GPIO19
-DHT dht3(17, DHT11);   // GPIO17
-DHT dht4(16, DHT11);   // GPIO16
+DHT dht1(DHT11_1_PIN, DHT11);   
+DHT dht2(DHT11_2_PIN, DHT11);   
+DHT dht3(DHT11_3_PIN, DHT11);   
+DHT dht4(DHT11_4_PIN, DHT11);   
 
-/* variables para almacenar datos de DHT11 */
+/* DHT11 data acquisition */
 float temp1, temp2, temp3, temp4;
 float tempAvg;
 
-/* humedad en tierra */
+/* Soil humidity */
 uint16_t gndHum1, gndHum2, gndHum3, gndHum4;
 uint16_t gndHumAvg;
 float gndHumAvg_percent;
-int16_t gndHum1_mapped; 
+int16_t gndHum1_mapped, gndHum2_mapped, gndHum3_mapped, gndHum4_mapped;
 
-/* Control del tiempo para ejecucion no bloqueante */
+/* Time control (non-blocking execution) */
 unsigned long currentTime  = 0;
+
 unsigned long lastDHTReadTime = 0;
 unsigned long lastADCReadTime = 0;
 unsigned long lastHUMReadTime = 0;
 unsigned long lastTimeUpdate = 0;
 
-/* Intervalos de lectura */
-const unsigned long ADC_INTERVAL = 1000;  // 1s
+/* Reading intervals */
+long DAC_INTERVAL = 0;
 const unsigned long DHT_INTERVAL = 2000;  
 const unsigned long HUM_INTERVAL = 2000;  
-const unsigned long TIME_UPDATE_INTERVAL = 60000; // 1 minuto
+const unsigned long TIME_UPDATE_INTERVAL = 60000; // 1 minute
 
-/* Control de riego no bloqueante relay 3 */
+/* Non-blocking irrigation control */
 bool irrigationIsActive = false;
 unsigned long irrigationStartTime = 0;
 const uint16_t IRRIGATION_DURATION = 7000;
 
-/* config WiFi y NTP */ //"INFINITUM6667_2.4"; //"Py8gEB84up";
+/* WiFi credentials setup */ 
 const char* ssid     = "Alumnos";        
 const char* password = "30L0x!4Lu$";
 
-/* NTP y GMT */
+/* NTP and GMT */
 const long gmtOffset_sec = -6 * 3600;   // UTC-6 (CDMX, GDL)
 const int daylightOffset_sec = 0;
 const char *ntpServer = "pool.ntp.org";
 
-/* actualizacion de zona horaria */
+/* GMT Time zone update */
 uint8_t currentHour;
 uint8_t currentMinute;
 uint8_t currentSecond;
 
-/* Relays setup */
-void relaysInit()
-{
-  pinMode(RELAY1_PIN, OUTPUT);
-  pinMode(RELAY2_PIN, OUTPUT);
-  pinMode(RELAY3_PIN, OUTPUT);
-  pinMode(RELAY4_PIN, OUTPUT);
+/* Scan and Ramp setup for potentiostat */
+uint8_t val = 0;
+float c = 0;
+uint8_t n = 0;
+uint16_t pos = 0;
+float Potstep = 0.0078;
+int vevals[] = {100,20,50,100,200,250,300};
+const uint8_t count = 6;
+long intervalos[7];
 
-  digitalWrite(RELAY1_PIN, HIGH);
-  digitalWrite(RELAY2_PIN, HIGH);
-  digitalWrite(RELAY3_PIN, HIGH);
-  digitalWrite(RELAY4_PIN, HIGH);
+/* status flags to set potentiostat ramp direction */
+bool forward = true;
+bool scanning = false;
+
+/* This function pre-calculates the intervals needed to
+ * perform different scan rates (mV) for the potentiostat ramp 
+ * and it setups the initial ramp  
+ */
+void Setup_potentiostatRamp_NonBlocking(void) 
+{
+  dacWrite(DAC_PIN, 0);
+  /* precalculo de intervalos para cada scan rate */
+  for(pos = 0; pos < count; pos++)
+  {
+    intervalos[pos] = 10000L / (vevals[pos] * 128L);
+  }
+  pos = 0;
+  n = 0;
+  val = 0;
+  scanning = true;
+  forward = true;
+  lastADCReadTime = millis();
+  DAC_INTERVAL = intervalos[pos] / 1000;
 }
+
+/* This function executes the forward and reverse scan
+ * generating the desired ramp in order to test the 
+ * potentiostat and behavior on the electrochemical cells  
+ */
+void Potentiostat_Scan_NonBlocking(void)
+{
+  if(scanning)
+  {
+    if(currentTime - lastADCReadTime >= DAC_INTERVAL)
+    {
+      lastADCReadTime = currentTime;
+      dacWrite(DAC_PIN, val);
+      c = analogRead(ADC_PIN);
+      Serial.print(val);
+      Serial.print(" ");
+      Serial.print(c);
+      Serial.print(" ");
+      Serial.print(n);
+      Serial.print(" ");
+      Serial.print(vevals[pos]);
+      Serial.print(" ");
+      Serial.println(DAC_INTERVAL);
+
+      /* Update DAC value */
+      if(forward)
+      {
+        val++;
+        if(val > 255)
+        {
+          val = 255;
+          forward = false;
+        }
+      }
+      else
+      {
+        val--;
+        if(val < 0)
+        {
+          val = 0;
+          forward = true;
+          n++;
+          if(n > 1)
+          {
+            pos++;
+            if(pos >= count)
+            {
+              pos = 0;             /* to repeat scan */
+              //scanning = false;  /* to end scan */
+            }
+            n = 0;
+            DAC_INTERVAL = intervalos[pos] / 1000;
+          }
+        }
+      }
+    }
+  }
+}
+
+/*
+ * This function configures and initializes all the relay modules
+ * needed to control all boolean logic that ESP32 can handle directly 
+ * by its GPIOs
+ */
+void relaysInit(void)
+{
+  pinMode(PELTIER_HOT_PIN,      OUTPUT);
+  pinMode(PELTIER_HOT_FAN_PIN,  OUTPUT);
+  pinMode(PELTIER_COLD_PIN,     OUTPUT);
+  pinMode(PELTIER_COLD_FAN_PIN, OUTPUT);
+  pinMode(WATER_PUMP_PIN,       OUTPUT);
+  pinMode(RGB_STRIPE_RED_PIN,   OUTPUT);
+  pinMode(RGB_STRIPE_BLUE_PIN,  OUTPUT);
+  pinMode(RGB_STRIPE_GREEN_PIN, OUTPUT);
+
+  digitalWrite(PELTIER_HOT_PIN,      OFF);
+  digitalWrite(PELTIER_HOT_FAN_PIN,  OFF);
+  digitalWrite(PELTIER_COLD_PIN,     OFF);
+  digitalWrite(PELTIER_COLD_FAN_PIN, OFF);
+  digitalWrite(WATER_PUMP_PIN,       OFF);
+  digitalWrite(RGB_STRIPE_RED_PIN,   OFF);
+  digitalWrite(RGB_STRIPE_BLUE_PIN,  OFF);
+  digitalWrite(RGB_STRIPE_GREEN_PIN, OFF);
+}
+
+
 
 /* Main setup ***********************************************************/
 void setup() 
 {
+  /* Serial init */
   Serial.begin(115200);
 
   /* ADC setup */
   analogReadResolution(12);  // 0-4095
+
+  /* Potentiostat setup */
+  Setup_potentiostatRamp_NonBlocking();
 
   /* relays setup*/
   relaysInit();
@@ -106,36 +232,41 @@ void setup()
 
   /* WiFi setup */
   WiFi.begin(ssid, password);
-  Serial.print("Conectando...");
+  Serial.print("Connecting...");
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("WiFi conectado");
+  Serial.println("WiFi connected");
 
-  /* NTP Setup init y espera a sincronizar hora */
+  /* NTP init and wait till time is synced */
   configTime(gmtOffset_sec, daylightOffset_sec, "pool.ntp.org", "time.nist.gov");
   struct tm timeinfo;
-  Serial.println("Esperando hora NTP...");
+  Serial.println("Waiting for NTP server...");
   while (!getLocalTime(&timeinfo))
   {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("\nHora sincronizada");
+  Serial.println("\nTime properly sync");
 }
 
 
 /* App entry loop ******************************************/
 void loop() 
 {
+  /* Aactual execution time acquisition in order toperform non-blocking tasks */
   currentTime = millis();
-   /*************************** Actualizacion de hora cada minuto */
+
+  /*************************** Potentiostat ADC reading according to set interval */
+  /* Concurrent potentiostat ramp execution */
+  Potentiostat_Scan_NonBlocking();
+
+  /*************************** Time update each minute */
   if (currentTime - lastTimeUpdate >= TIME_UPDATE_INTERVAL)
   {
     lastTimeUpdate = currentTime;
-
     struct tm timeinfo;
     if (getLocalTime(&timeinfo))
     {
@@ -143,121 +274,104 @@ void loop()
       currentMinute = timeinfo.tm_min;
       currentSecond = timeinfo.tm_sec;
 
-      Serial.printf("Hora actual: %02d:%02d:%02d\n", currentHour, currentMinute, currentSecond);
+      Serial.printf("Actual time: %02d:%02d:%02d\n", currentHour, currentMinute, currentSecond);
 
-      /* === Control del RELAY 4 (luz RGB blanca) === */
-      /* enciende a las 7am, se apaga a las 8pm */
+      /* === RGB LED Stripe control === */
+      /* Turns on at 7am, and goes off at 8pm */
       if (currentHour >= 7 && currentHour < 20)
       {
-        digitalWrite(RELAY4_PIN, LOW);  // Encender (activo LOW)
-        Serial.println("Luz encendida (RELAY 4 ON)");
+        
+        digitalWrite(RGB_STRIPE_RED_PIN, ON);
+        digitalWrite(RGB_STRIPE_BLUE_PIN, ON);
+        digitalWrite(RGB_STRIPE_GREEN_PIN, ON);  
+        Serial.println("7am, lights on...");
       }
       else
       {
-        digitalWrite(RELAY4_PIN, HIGH); // Apagar
-        Serial.println("Luz apagada (RELAY 4 OFF)");
+        digitalWrite(RGB_STRIPE_RED_PIN, OFF);
+        digitalWrite(RGB_STRIPE_BLUE_PIN, OFF);
+        digitalWrite(RGB_STRIPE_GREEN_PIN, OFF);  
+        Serial.println("8pm, lights off...");
       }
     }
     else
     {
-      Serial.println("Fallo al actualizar hora");
+      Serial.println("Time sync failed...");
     }
   }
 
-  /*************************** lectura ADC potenciostato de acuerdo al intervalo establecido */
-  if (currentTime - lastADCReadTime >= ADC_INTERVAL) 
-  {
-    // === Lectura ADC ===
-    lastADCReadTime = currentTime;
-    sensorValue = analogRead(34);
-    voltage = sensorValue * (3.3 / 4095.0);
-    Serial.print("ADC -> Valor: ");
-    Serial.print(sensorValue);
-    Serial.print(" - Voltaje: ");
-    Serial.println(voltage, 3);
-  }
-
-  /**************************** lectura DHT11 de acuerdo al intervalo establecido */
+  /**************************** DHT11 reading according to set interval */
   if (currentTime - lastDHTReadTime >= DHT_INTERVAL) 
   {
     lastDHTReadTime = currentTime;
 
-    // === Lectura DHT11 === //
+    /* === DHT11 reading === */
     temp1 = dht1.readTemperature(); 
     temp2 = dht2.readTemperature();  
     temp3 = dht3.readTemperature();  
     temp4 = dht4.readTemperature();  
 
-    /* Temperatura promedio DHT11 */
+    /* DHT11 average temperature */
     tempAvg = (temp1 + temp2 + temp3 + temp4) / 4.0;
-    Serial.print("Temperatura promedio: "); Serial.print(tempAvg); Serial.println(" °C");
+    Serial.print("Average temperature: "); Serial.print(tempAvg); Serial.println(" °C");
 
-    /* Control ante temperatura */
+    /* Temperature control logic */
     if(tempAvg < 20.0)
     {
-      digitalWrite(RELAY1_PIN, LOW);
-      Serial.println("Relay 1 ON");
+      digitalWrite(PELTIER_HOT_PIN,     ON);
+      digitalWrite(PELTIER_HOT_FAN_PIN, ON);
+      Serial.println("Peltier 1 warming...(FAN ON)");
     }
     else
     {
-      digitalWrite(RELAY1_PIN, HIGH);
-      Serial.println("Relay 1 OFF");
+      digitalWrite(PELTIER_HOT_PIN,     OFF);
+      digitalWrite(PELTIER_HOT_FAN_PIN, OFF);
+      Serial.println("Peltier 1 OFF...(FAN OFF)");
     }
 
     if(tempAvg > 30.0)
     {
-      digitalWrite(RELAY2_PIN, LOW);
-      Serial.println("Relay 2 ON");
+      digitalWrite(PELTIER_COLD_PIN,     ON);
+      digitalWrite(PELTIER_COLD_FAN_PIN, ON);
+      Serial.println("Peltier 2 cooling...(FAN ON)");
     }
     else
     {
-      digitalWrite(RELAY2_PIN, HIGH);
-      Serial.println("Relay 2 OFF");
+      digitalWrite(PELTIER_COLD_PIN,     OFF);
+      digitalWrite(PELTIER_COLD_FAN_PIN, OFF);
+      Serial.println("Peltier 2 OFF...(FAN OFF)");
     }
   }
 
-  /**************************** lectura humedad en tierra de acuerdo al intervalo establecido */
+  /**************************** Soil humidity reading according to set interval */
   if(currentTime - lastHUMReadTime >= HUM_INTERVAL)
   {
     lastHUMReadTime = currentTime;
-    /* === Lectura de humedad en tierra === */
+    /* === Soil moisture reading === */
     gndHum1 = analogRead(HUM_SENSOR1_PIN);
-    //gndHum2 = analogRead(HUM_SENSOR2_PIN);
-    //gndHum3 = analogRead(HUM_SENSOR3_PIN);
-    //gndHum4 = analogRead(HUM_SENSOR4_PIN);
 
-    // Mapeo de lecturas a porcentaje en cada sensor
+    /* Humidity sensors reading mapped to percentage */
     gndHum1_mapped = map(gndHum1, 0, 4095, 100, 0);
-  /*gndHum2_mapped = map(gndHum2, 0, 4095, 100, 0);
-    gndHum3_mapped = map(gndHum3, 0, 4095, 100, 0);
-    gndHum4_mapped = map(gndHum4, 0, 4095, 100, 0); */
 
-    /* Promedio de humedad en tierra de los 4 sensores mapeados */
-    //gndHumAvg = (gndHum1_mapped + gndHum2_mapped + gndHum3_mapped + gndHum4_mapped) / 4;
-    //gndHumAvg_percent = (gndHumAvg * 100.0) / 4095.0;
-    //Serial.print("Humedad en tierra promedio: "); Serial.print(gndHumAvg_percent, 2); Serial.println(" %");
-    // PROVISIONAL
-    Serial.print("Humedad sensor 1: ");
-    Serial.print(gndHum1_mapped);
-    Serial.println("%");
-    /* Control ante humedad en tierra, activa riego si humedad global baja */
-   /* if(!irrigationIsActive && gndHumAvg_percent < 30.0)
+    Serial.print("Humidity sensor 1: ");
+    Serial.print(gndHum1_mapped); Serial.println("%");
+    
+    /* Irrigation control due to soil humidity */
+    if(!irrigationIsActive && gndHum1_mapped < 30.0)
     {
-      digitalWrite(RELAY3_PIN, LOW);
+      digitalWrite(WATER_PUMP_PIN, ON);
       irrigationStartTime = currentTime;
       irrigationIsActive = true;
-      Serial.println("RELAY 3 ON");
-    }*/
+      Serial.println("Watering dry soil...(7s)");
+    }
   }
-  /* riego finaliza tras 7 segundos */
- /* if(irrigationIsActive && (currentTime - irrigationStartTime >= IRRIGATION_DURATION))
+  /* Irrigation ends past 7 seconds */
+  if(irrigationIsActive && (currentTime - irrigationStartTime >= IRRIGATION_DURATION))
   {
-    digitalWrite(RELAY3_PIN, LOW);
+    digitalWrite(WATER_PUMP_PIN, OFF);
     irrigationIsActive = false;
-    Serial.println("RELAY 3 OFF");
-  }*/
+    Serial.println("Irrigation completed");
+  }
 }
-
-
 
 
