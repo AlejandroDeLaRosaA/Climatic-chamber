33913d6fe0d63aa7ee8e4f5166d2de589ab2fbd3
Alejandro DLR <114164738+AlejandroDeLaRosaA@users.noreply.github.com>
2025-05-02 21:50:21 -0600

    Updated windows, added voltammetry tab

--- GUI_full.py
+++ GUI_full.py
@@ -2,189 +2,161 @@ import sys
 import random
 from PyQt5.QtWidgets import (
     QApplication, QSpacerItem, QSizePolicy, QWidget, QTabWidget, QVBoxLayout, QPushButton, QLabel,
-    QMainWindow, QStackedWidget, QTableWidget, QTableWidgetItem, QHBoxLayout
-)
+    QMainWindow, QStackedWidget, QTableWidget, QTableWidgetItem, QHBoxLayout 
+    )
 from PyQt5.QtCore import Qt, QTimer, QTime
 from PyQt5.QtGui import QColor, QPixmap
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 
-
+#########################################
 # Utilidad para crear gráficas en widgets
-class LivePlotCanvas(FigureCanvas):
-    def __init__(self, title, ylabel, color='tab:blue', parent=None, width=5, height=2, dpi=70):
+class MultiLinePlotCanvas(FigureCanvas):
+    def __init__(self, title, ylabel, lines_info, parent=None, width=5, height=3.2, dpi=100):
         fig = Figure(figsize=(width, height), dpi=dpi)
         self.ax = fig.add_subplot(111)
         self.ax.set_title(title)
-        self.ax.set_xlabel("Tiempo")
+        self.ax.set_xlabel("Tiempo (s)")
         self.ax.set_ylabel(ylabel)
         self.ax.grid(True)
-        self.line, = self.ax.plot([], [], color=color)
-        self.data = []
+        self.lines = {}
+        self.data = {}
         self.time = []
+
+        for label, color in lines_info:
+            line, = self.ax.plot([], [], label=label, color=color)
+            self.lines[label] = line
+            self.data[label] = []
+
+        self.ax.legend(loc='upper right')
         super().__init__(fig)
 
-    def update_plot(self, new_val):
-        self.data.append(new_val)
+    def update_plot(self, new_values: dict):
         self.time.append(len(self.time))
-        if len(self.data) > 50:
-            self.data = self.data[-50:]
+        for label, val in new_values.items():
+            self.data[label].append(val)
+            if len(self.data[label]) > 50:
+                self.data[label] = self.data[label][-50:]
+        if len(self.time) > 50:
             self.time = self.time[-50:]
-        self.line.set_data(self.time, self.data)
+        for label, line in self.lines.items():
+            line.set_data(self.time, self.data[label])
         self.ax.set_xlim(max(0, self.time[0]), self.time[-1] + 1)
-        self.ax.set_ylim(min(self.data) - 1, max(self.data) + 1)
+        all_vals = [v for sublist in self.data.values() for v in sublist]
+        if all_vals:
+            self.ax.set_ylim(min(all_vals) - 1, max(all_vals) + 1)
+        self.figure.tight_layout()
         self.draw()
+        
 
 
+################################
 class TemperatureTab(QWidget):
     def __init__(self, go_back_callback):
         super().__init__()
         self.graph_view = True
 
-        # Crear la figura y los ejes para múltiples líneas
-        self.fig = Figure(figsize=(5, 2), dpi=100)
-        self.ax = self.fig.add_subplot(111)
-        self.ax.set_title("Temperatura Global e Individual")
-        self.ax.set_xlabel("Tiempo")
-        self.ax.set_ylabel("°C")
-        self.ax.grid(True)
-
-        # Líneas para promedio y 4 sensores
-        self.line_avg, = self.ax.plot([], [], label='Promedio', color='tab:red')
-        self.line_s1, = self.ax.plot([], [], label='Sensor 1', color='tab:blue')
-        self.line_s2, = self.ax.plot([], [], label='Sensor 2', color='tab:green')
-        self.line_s3, = self.ax.plot([], [], label='Sensor 3', color='tab:orange')
-        self.line_s4, = self.ax.plot([], [], label='Sensor 4', color='tab:purple')
-
-        self.ax.legend(loc='upper right')
-
-        self.canvas = FigureCanvas(self.fig)
-
-        self.data_avg = []
-        self.data_s1 = []
-        self.data_s2 = []
-        self.data_s3 = []
-        self.data_s4 = []
-        self.time = []
-
+        self.plot = MultiLinePlotCanvas("Temperatura", "Grados °C", [
+            ("Promedio", 'tab:red'),
+            ("Sensor 1", 'tab:blue'),
+            ("Sensor 2", 'tab:orange'),
+            ("Sensor 3", 'tab:green'),
+            ("Sensor 4", 'tab:purple'),
+        ])
         self.table = QTableWidget()
+
         self.toggle_btn = QPushButton("Mostrar como tabla")
+        self.toggle_btn.setFixedWidth(200)
         self.toggle_btn.clicked.connect(self.toggle_view)
 
         self.back_btn = QPushButton("Volver al Menú Principal")
+        self.back_btn.setFixedWidth(200)
         self.back_btn.clicked.connect(go_back_callback)
 
+        self.btn_layout = QHBoxLayout()
+        self.btn_layout.addWidget(self.toggle_btn)
+        self.btn_layout.addWidget(self.back_btn)
+        self.btn_container = QWidget()
+        self.btn_container.setLayout(self.btn_layout)
+
         self.layout = QVBoxLayout()
-        self.layout.addWidget(self.toggle_btn)
-        self.layout.addWidget(self.canvas)
-        self.layout.addWidget(self.back_btn)
+        self.layout.addWidget(self.plot)
+        self.layout.addWidget(self.table)
+        self.layout.addWidget(self.btn_container, alignment=Qt.AlignCenter)
         self.setLayout(self.layout)
+        self.table.hide()
 
         self.timer = QTimer()
         self.timer.timeout.connect(self.simulate_data)
         self.timer.start(1000)
 
     def simulate_data(self):
-        s1 = round(random.uniform(20.0, 30.0), 2)
-        s2 = round(random.uniform(20.0, 30.0), 2)
-        s3 = round(random.uniform(20.0, 30.0), 2)
-        s4 = round(random.uniform(20.0, 30.0), 2)
-        avg = round((s1 + s2 + s3 + s4) / 4, 2)
-
-        self.data_s1.append(s1)
-        self.data_s2.append(s2)
-        self.data_s3.append(s3)
-        self.data_s4.append(s4)
-        self.data_avg.append(avg)
-        self.time.append(len(self.time))
-
-        if len(self.time) > 50:
-            self.time = self.time[-50:]
-            self.data_avg = self.data_avg[-50:]
-            self.data_s1 = self.data_s1[-50:]
-            self.data_s2 = self.data_s2[-50:]
-            self.data_s3 = self.data_s3[-50:]
-            self.data_s4 = self.data_s4[-50:]
-
-        self.line_avg.set_data(self.time, self.data_avg)
-        self.line_s1.set_data(self.time, self.data_s1)
-        self.line_s2.set_data(self.time, self.data_s2)
-        self.line_s3.set_data(self.time, self.data_s3)
-        self.line_s4.set_data(self.time, self.data_s4)
-
-        self.ax.set_xlim(max(0, self.time[0]), self.time[-1] + 1)
-        all_data = self.data_avg + self.data_s1 + self.data_s2 + self.data_s3 + self.data_s4
-        self.ax.set_ylim(min(all_data) - 1, max(all_data) + 1)
-        self.canvas.draw()
-
+        data = {
+            "Promedio": round(random.uniform(22.0, 28.0), 2),
+            "Sensor 1": round(random.uniform(21.0, 30.0), 2),
+            "Sensor 2": round(random.uniform(21.0, 30.0), 2),
+            "Sensor 3": round(random.uniform(21.0, 30.0), 2),
+            "Sensor 4": round(random.uniform(21.0, 30.0), 2),
+        }
+        self.plot.update_plot(data)
         if not self.graph_view:
-            self.update_table()
+            self.update_table(data)
 
     def toggle_view(self):
         self.graph_view = not self.graph_view
         if self.graph_view:
-            self.layout.replaceWidget(self.table, self.canvas)
             self.table.hide()
-            self.canvas.show()
+            self.plot.show()
             self.toggle_btn.setText("Mostrar como tabla")
         else:
             self.update_table()
-            self.layout.replaceWidget(self.canvas, self.table)
-            self.canvas.hide()
+            self.plot.hide()
             self.table.show()
             self.toggle_btn.setText("Mostrar como gráfica")
 
-    def update_table(self):
-        self.table.setRowCount(len(self.data_avg))
+    def update_table(self, latest_data=None):
+        num_rows = len(self.plot.time)
+        self.table.setRowCount(num_rows)
         self.table.setColumnCount(5)
-        self.table.setHorizontalHeaderLabels(["Promedio (°C)", "Sensor 1", "Sensor 2", "Sensor 3", "Sensor 4"])
-        for i in range(len(self.data_avg)):
-            self.table.setItem(i, 0, QTableWidgetItem(str(self.data_avg[i])))
-            self.table.setItem(i, 1, QTableWidgetItem(str(self.data_s1[i])))
-            self.table.setItem(i, 2, QTableWidgetItem(str(self.data_s2[i])))
-            self.table.setItem(i, 3, QTableWidgetItem(str(self.data_s3[i])))
-            self.table.setItem(i, 4, QTableWidgetItem(str(self.data_s4[i])))
+        self.table.setHorizontalHeaderLabels(["Promedio", "Sensor 1", "Sensor 2", "Sensor 3", "Sensor 4"])
+        for i in range(num_rows):
+            for j, key in enumerate(["Promedio", "Sensor 1", "Sensor 2", "Sensor 3", "Sensor 4"]):
+                val = self.plot.data[key][i] if i < len(self.plot.data[key]) else ''
+                self.table.setItem(i, j, QTableWidgetItem(str(val)))
 
+################################
 class SoilHumidityTab(QWidget):
     def __init__(self, go_back_callback):
         super().__init__()
         self.graph_view = True
 
-        # Crear la figura y ejes
-        self.fig = Figure(figsize=(5, 2), dpi=100)
-        self.ax = self.fig.add_subplot(111)
-        self.ax.set_title("Humedad en Tierra")
-        self.ax.set_xlabel("Tiempo")
-        self.ax.set_ylabel("%")
-        self.ax.grid(True)
-
-        # Líneas para los sensores y el promedio
-        self.line_s1, = self.ax.plot([], [], label='Sensor 1', color='tab:blue')
-        self.line_s2, = self.ax.plot([], [], label='Sensor 2', color='tab:orange')
-        self.line_avg, = self.ax.plot([], [], label='Promedio', color='tab:green')
-
-        self.ax.legend(loc='upper right')
-
-        self.canvas = FigureCanvas(self.fig)
-
-        # Almacenes de datos
-        self.data_s1 = []
-        self.data_s2 = []
-        self.data_avg = []
-        self.time = []
-
+        self.plot = MultiLinePlotCanvas("Humedad en Tierra", "Porcentaje de humedad %", [
+            ("Promedio", 'tab:green'),
+            ("Sensor 1", 'tab:blue'),
+            ("Sensor 2", 'tab:orange'),
+        ])
         self.table = QTableWidget()
+
         self.toggle_btn = QPushButton("Mostrar como tabla")
+        self.toggle_btn.setFixedWidth(200)
         self.toggle_btn.clicked.connect(self.toggle_view)
 
         self.back_btn = QPushButton("Volver al Menú Principal")
+        self.back_btn.setFixedWidth(200)
         self.back_btn.clicked.connect(go_back_callback)
 
+        self.btn_layout = QHBoxLayout()
+        self.btn_layout.addWidget(self.toggle_btn)
+        self.btn_layout.addWidget(self.back_btn)
+        self.btn_container = QWidget()
+        self.btn_container.setLayout(self.btn_layout)
+
         self.layout = QVBoxLayout()
-        self.layout.addWidget(self.toggle_btn)
-        self.layout.addWidget(self.canvas)
-        self.layout.addWidget(self.back_btn)
+        self.layout.addWidget(self.plot)
+        self.layout.addWidget(self.table)
+        self.layout.addWidget(self.btn_container, alignment=Qt.AlignCenter)
         self.setLayout(self.layout)
+        self.table.hide()
 
         self.timer = QTimer()
         self.timer.timeout.connect(self.simulate_data)
@@ -195,56 +167,39 @@ class SoilHumidityTab(QWidget):
         val2 = round(random.uniform(30.0, 70.0), 2)
         avg = round((val1 + val2) / 2, 2)
 
-        self.data_s1.append(val1)
-        self.data_s2.append(val2)
-        self.data_avg.append(avg)
-        self.time.append(len(self.time))
-
-        if len(self.time) > 50:
-            self.data_s1 = self.data_s1[-50:]
-            self.data_s2 = self.data_s2[-50:]
-            self.data_avg = self.data_avg[-50:]
-            self.time = self.time[-50:]
-
-        self.line_s1.set_data(self.time, self.data_s1)
-        self.line_s2.set_data(self.time, self.data_s2)
-        self.line_avg.set_data(self.time, self.data_avg)
-
-        all_data = self.data_s1 + self.data_s2 + self.data_avg
-        self.ax.set_xlim(max(0, self.time[0]), self.time[-1] + 1)
-        self.ax.set_ylim(min(all_data) - 1, max(all_data) + 1)
-        self.canvas.draw()
-
+        data = {
+            "Promedio": avg,
+            "Sensor 1": val1,
+            "Sensor 2": val2
+        }
+        self.plot.update_plot(data)
         if not self.graph_view:
             self.update_table()
 
     def toggle_view(self):
         self.graph_view = not self.graph_view
         if self.graph_view:
-            self.layout.replaceWidget(self.table, self.canvas)
             self.table.hide()
-            self.canvas.show()
+            self.plot.show()
             self.toggle_btn.setText("Mostrar como tabla")
         else:
             self.update_table()
-            self.layout.replaceWidget(self.canvas, self.table)
-            self.canvas.hide()
+            self.plot.hide()
             self.table.show()
             self.toggle_btn.setText("Mostrar como gráfica")
 
     def update_table(self):
-        num_rows = len(self.data_avg)
+        num_rows = len(self.plot.time)
         self.table.setRowCount(num_rows)
         self.table.setColumnCount(3)
         self.table.setHorizontalHeaderLabels(["Sensor 1 (%)", "Sensor 2 (%)", "Promedio (%)"])
         for i in range(num_rows):
-            self.table.setItem(i, 0, QTableWidgetItem(str(self.data_s1[i])))
-            self.table.setItem(i, 1, QTableWidgetItem(str(self.data_s2[i])))
-            self.table.setItem(i, 2, QTableWidgetItem(str(self.data_avg[i])))
-
-  
+            for j, key in enumerate(["Sensor 1", "Sensor 2", "Promedio"]):
+                val = self.plot.data[key][i] if i < len(self.plot.data[key]) else ''
+                self.table.setItem(i, j, QTableWidgetItem(str(val)))
 
 
+##################################
 class LightIrrigationTab(QWidget):
     def __init__(self, go_back_callback):
         super().__init__()
@@ -284,35 +239,75 @@ class LightIrrigationTab(QWidget):
                 lbl.setStyleSheet("background-color: red; color: white; font-size: 16px;")
 
 
+##############################
+class VoltammetryTab(QWidget):
+    def __init__(self, go_back_callback):
+        super().__init__()
+
+
+
+        # Tres gráficas: DAC (V), ADC (V), Tiempo (s)
+        self.plot_dac = MultiLinePlotCanvas("Voltaje de Salida (DAC)", "Voltios (V)", [("DAC", 'tab:blue')])
+        self.plot_adc = MultiLinePlotCanvas("Corriente Leída (ADC)", "Voltios (V)", [("ADC", 'tab:orange')])
+        self.plot_time = MultiLinePlotCanvas("Intervalo de Tiempo", "Tiempo (ms)", [("Intervalo", 'tab:green')])
+
+
+        self.back_btn = QPushButton("Volver al Menú Principal")
+        self.back_btn.setFixedWidth(200)
+        self.back_btn.clicked.connect(go_back_callback)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.plot_dac)
+        layout.addWidget(self.plot_adc)
+        layout.addWidget(self.plot_time)
+        layout.addWidget(self.back_btn, alignment=Qt.AlignCenter)
+        self.setLayout(layout)
+
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.simulate_voltammetry_data)
+        self.timer.start(1000)
+
+    def simulate_voltammetry_data(self):
+        # Simulación para fines de prueba
+        dac_val = round(random.uniform(-1.0, 1.0), 3)
+        adc_val = round(random.uniform(0.0, 3.3), 3)
+        interval = random.randint(90, 110)
+
+        self.plot_dac.update_plot({"DAC": dac_val})
+        self.plot_adc.update_plot({"ADC": adc_val})
+        self.plot_time.update_plot({"Intervalo": interval})
+
+############################
 class MainMenu(QWidget):
-    def __init__(self, go_temp, go_hum, go_states):
+    def __init__(self, go_temp, go_hum, go_states, go_voltammetry):
         super().__init__()
 
         # Fondo con imagen
         self.background = QLabel(self)
-        pixmap = QPixmap("C:/Users/Aleja/Desktop/GUI_Alex/ciatej_logo.png")  # <-- cambia a la ruta de tu imagen si está en otro lugar
+        pixmap = QPixmap("C:/Users/Aleja/Desktop/GUI_Alex/ciatej_logo.png") 
         self.background.setPixmap(pixmap)
         self.background.setScaledContents(True)
-        self.background.lower()  # asegúrate de que el fondo esté detrás de todo
+        self.background.lower()  
 
         # Título
         self.title = QLabel("Cámara de crecimiento")
         self.title.setAlignment(Qt.AlignCenter)
         self.title.setStyleSheet("font-size: 28px; font-weight: bold; margin-top: 40px; color: white;")
-
-        # Botones más estrechos (1/3 del ancho)
         self.temp_btn = QPushButton("Ver Temperatura")
         self.hum_btn = QPushButton("Ver Humedad en Tierra")
         self.states_btn = QPushButton("Ver Estados del Sistema")
+        self.voltammetry_btn = QPushButton("Ver Voltametría")
 
         for btn in [self.temp_btn, self.hum_btn, self.states_btn]:
-            btn.setFixedWidth(250)  # Ancho fijo (ajustable según ventana)
+            btn.setFixedWidth(250)  
             btn.setStyleSheet("font-size: 16px; padding: 10px;")
             btn.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
 
         self.temp_btn.clicked.connect(go_temp)
         self.hum_btn.clicked.connect(go_hum)
         self.states_btn.clicked.connect(go_states)
+        self.voltammetry_btn.clicked.connect(go_voltammetry)
+
 
         self.clock_label = QLabel("Hora actual: --:--:--")
         self.clock_label.setAlignment(Qt.AlignCenter)
@@ -324,6 +319,7 @@ class MainMenu(QWidget):
         button_layout.addWidget(self.temp_btn)
         button_layout.addWidget(self.hum_btn)
         button_layout.addWidget(self.states_btn)
+        button_layout.addWidget(self.voltammetry_btn)
 
         spacer = QSpacerItem(20, 80, QSizePolicy.Minimum, QSizePolicy.Fixed)
         main_layout = QVBoxLayout()
@@ -347,7 +343,7 @@ class MainMenu(QWidget):
         now = QTime.currentTime()
         self.clock_label.setText(f"Hora actual: {now.toString('HH:mm:ss')}")
 
-
+##############################
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -360,16 +356,21 @@ class MainWindow(QMainWindow):
         self.main_menu = MainMenu(
             self.show_temp_tab,
             self.show_hum_tab,
-            self.show_states_tab
+            self.show_states_tab,
+            self.show_voltammetry_tab
         )
         self.temp_tab = TemperatureTab(self.show_main_menu)
         self.hum_tab = SoilHumidityTab(self.show_main_menu)
         self.states_tab = LightIrrigationTab(self.show_main_menu)
+        self.voltammetry_tab = VoltammetryTab(self.show_main_menu)
+
 
         self.stack.addWidget(self.main_menu)
         self.stack.addWidget(self.temp_tab)
         self.stack.addWidget(self.hum_tab)
         self.stack.addWidget(self.states_tab)
+        self.stack.addWidget(self.voltammetry_tab)
+
 
     def show_main_menu(self):
         self.stack.setCurrentWidget(self.main_menu)
@@ -383,10 +384,13 @@ class MainWindow(QMainWindow):
     def show_states_tab(self):
         self.stack.setCurrentWidget(self.states_tab)
 
+    def show_voltammetry_tab(self):
+        self.stack.setCurrentWidget(self.voltammetry_tab)
 
+
+# ******** app entry ******** #
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
-
